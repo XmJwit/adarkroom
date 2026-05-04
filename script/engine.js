04637dbf95cb95fb86c586526d2023f13724a918
Xu <36185379+XmJwit@users.noreply.github.com>
2026-05-04 16:07:30 +0000

    Add Chinese localization for new game content and comment out social sharing features
    - Added translations for various in-game items and descriptions in Chinese.
    - Commented out the GitHub link and social sharing functionalities in the engine script to streamline the user interface.

--- script/engine.js
+++ script/engine.js
@@ -194,11 +194,11 @@
           .appendTo(menu);
       }
 
-      $('<span>')
-        .addClass('menuBtn')
-        .text(_('github.'))
-        .click(function() { window.open('https://github.com/doublespeakgames/adarkroom'); })
-        .appendTo(menu);
+      // $('<span>')
+      //   .addClass('menuBtn')
+      //   .text(_('github.'))
+      //   .click(function() { window.open('https://github.com/doublespeakgames/adarkroom'); })
+      //   .appendTo(menu);
 
       // Register keypress handlers
       $('body').off('keydown').keydown(Engine.keyDown);
@@ -437,84 +437,84 @@
       }
     },
 
-    getApp: function() {
-      Events.startEvent({
-        title: _('Get the App'),
-        scenes: {
-          start: {
-            text: [_('bring the room with you.')],
-            buttons: {
-              'ios': {
-                text: _('ios'),
-                nextScene: 'end',
-                onChoose: function () {
-                  window.open('https://itunes.apple.com/app/apple-store/id736683061?pt=2073437&ct=adrproper&mt=8');
-                }
-              },
-              'android': {
-                text: _('android'),
-                nextScene: 'end',
-                onChoose: function() {
-                  window.open('https://play.google.com/store/apps/details?id=com.yourcompany.adarkroom');
-                }
-              },
-              'close': {
-                text: _('close'),
-                nextScene: 'end'
-              }
-            }
-          }
-        }
-      });
-    },
+    // getApp: function() {
+    //   Events.startEvent({
+    //     title: _('Get the App'),
+    //     scenes: {
+    //       start: {
+    //         text: [_('bring the room with you.')],
+    //         buttons: {
+    //           'ios': {
+    //             text: _('ios'),
+    //             nextScene: 'end',
+    //             onChoose: function () {
+    //               window.open('https://itunes.apple.com/app/apple-store/id736683061?pt=2073437&ct=adrproper&mt=8');
+    //             }
+    //           },
+    //           'android': {
+    //             text: _('android'),
+    //             nextScene: 'end',
+    //             onChoose: function() {
+    //               window.open('https://play.google.com/store/apps/details?id=com.yourcompany.adarkroom');
+    //             }
+    //           },
+    //           'close': {
+    //             text: _('close'),
+    //             nextScene: 'end'
+    //           }
+    //         }
+    //       }
+    //     }
+    //   });
+    // },
 
-    share: function() {
-      Events.startEvent({
-        title: _('Share'),
-        scenes: {
-          start: {
-            text: [_('bring your friends.')],
-            buttons: {
-              'facebook': {
-                text: _('facebook'),
-                nextScene: 'end',
-                onChoose: function() {
-                  window.open('https://www.facebook.com/sharer/sharer.php?u=' + Engine.SITE_URL, 'sharer', 'width=626,height=436,location=no,menubar=no,resizable=no,scrollbars=no,status=no,toolbar=no');
-                }
-              },
-              'google': {
-                text:_('google+'),
-                nextScene: 'end',
-                onChoose: function() {
-                  window.open('https://plus.google.com/share?url=' + Engine.SITE_URL, 'sharer', 'width=480,height=436,location=no,menubar=no,resizable=no,scrollbars=no,status=no,toolbar=no');
-                }
-              },
-              'twitter': {
-                text: _('twitter'),
-                nextScene: 'end',
-                onChoose: function() {
-                  window.open('https://twitter.com/intent/tweet?text=A%20Dark%20Room&url=' + Engine.SITE_URL, 'sharer', 'width=660,height=260,location=no,menubar=no,resizable=no,scrollbars=yes,status=no,toolbar=no');
-                }
-              },
-              'reddit': {
-                text: _('reddit'),
-                nextScene: 'end',
-                onChoose: function() {
-                  window.open('http://www.reddit.com/submit?url=' + Engine.SITE_URL, 'sharer', 'width=960,height=700,location=no,menubar=no,resizable=no,scrollbars=yes,status=no,toolbar=no');
-                }
-              },
-              'close': {
-                text: _('close'),
-                nextScene: 'end'
-              }
-            }
-          }
-        }
-      },
-      {
-        width: '400px'
-      });
-    },
+    // share: function() {
+    //   Events.startEvent({
+    //     title: _('Share'),
+    //     scenes: {
+    //       start: {
+    //         text: [_('bring your friends.')],
+    //         buttons: {
+    //           'facebook': {
+    //             text: _('facebook'),
+    //             nextScene: 'end',
+    //             onChoose: function() {
+    //               window.open('https://www.facebook.com/sharer/sharer.php?u=' + Engine.SITE_URL, 'sharer', 'width=626,height=436,location=no,menubar=no,resizable=no,scrollbars=no,status=no,toolbar=no');
+    //             }
+    //           },
+    //           'google': {
+    //             text:_('google+'),
+    //             nextScene: 'end',
+    //             onChoose: function() {
+    //               window.open('https://plus.google.com/share?url=' + Engine.SITE_URL, 'sharer', 'width=480,height=436,location=no,menubar=no,resizable=no,scrollbars=no,status=no,toolbar=no');
+    //             }
+    //           },
+    //           'twitter': {
+    //             text: _('twitter'),
+    //             nextScene: 'end',
+    //             onChoose: function() {
+    //               window.open('https://twitter.com/intent/tweet?text=A%20Dark%20Room&url=' + Engine.SITE_URL, 'sharer', 'width=660,height=260,location=no,menubar=no,resizable=no,scrollbars=yes,status=no,toolbar=no');
+    //             }
+    //           },
+    //           'reddit': {
+    //             text: _('reddit'),
+    //             nextScene: 'end',
+    //             onChoose: function() {
+    //               window.open('http://www.reddit.com/submit?url=' + Engine.SITE_URL, 'sharer', 'width=960,height=700,location=no,menubar=no,resizable=no,scrollbars=yes,status=no,toolbar=no');
+    //             }
+    //           },
+    //           'close': {
+    //             text: _('close'),
+    //             nextScene: 'end'
+    //           }
+    //         }
+    //       }
+    //     }
+    //   },
+    //   {
+    //     width: '400px'
+    //   });
+    // },
 
     findStylesheet: function(title) {
       for(var i=0; i<document.styleSheets.length; i++) {
